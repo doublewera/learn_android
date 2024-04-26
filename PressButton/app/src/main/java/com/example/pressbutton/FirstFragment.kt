63d56eeabc3597fa344b95cc8617c29dcd89a731
Wera Barinova <alisawera@yandex.ru>
2024-04-26 21:38:33 +0300

    Добавлен таймер, по которому на текст-вью пишутся циферки

--- PressButton/app/src/main/java/com/example/pressbutton/FirstFragment.kt
+++ PressButton/app/src/main/java/com/example/pressbutton/FirstFragment.kt
@@ -1,12 +1,16 @@
 package com.example.pressbutton
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.pressbutton.databinding.FragmentFirstBinding
+import java.util.TimerTask
+import kotlin.concurrent.timer
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,6 +18,8 @@ import com.example.pressbutton.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+    private lateinit var timer: CountDownTimer
+    private var tm: Int = 0
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,15 +31,44 @@ class FirstFragment : Fragment() {
     ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        timer = object: CountDownTimer(20000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                tm++
+                addText(binding.textviewFirst, tm.toString())
+            }
+
+            override fun onFinish() {
+                addText(binding.textviewFirst,"Ura")
+            }
+        }
         return binding.root
 
     }
 
+    private fun addText(cmpnnt: TextView, txt: String) {
+        cmpnnt.text = cmpnnt.text.toString().plus(txt)
+    }
+
+    //private fun addText(cmpnnt: TextView, txt: String): TimerTask.() -> Unit {
+    //    return  {
+    //        cmpnnt.text = cmpnnt.text.toString().plus(txt)
+    //    }
+    //}
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.btnStart.setOnClickListener {
+            addText(binding.textviewFirst,"\nStart")
+            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            // Добавить код: создать файл, а если нет директории - создать директорию.
+            // Запустить фоновый процесс или "подписаться на циклическое событие по времени
+            timer.start()
+            //timer(initialDelay = 1000L, period = 1000L ,
+            //    action = addText(binding.textviewFirst, "Kuku")
+            //)
+            // Заменить надпись на кнопке на Pause, чтобы повторное нажатие приостанавливало треккинг координат
+            // ЕСЛИ уже идёт фоновый процесс, "отписаться от циклического события", либо "остановить фоновый"
+            //                                поменять на кнопке надпись на "продолжить" или "начать"
         }
     }
 
