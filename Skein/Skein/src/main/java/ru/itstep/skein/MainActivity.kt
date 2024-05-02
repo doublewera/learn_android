be8f14f8300146ddfc25107a37e3c026dad79e5e
Wera Barinova <alisawera@yandex.ru>
2024-05-02 13:18:13 +0300

    Location Permissions granted

--- Skein/Skein/src/main/java/ru/itstep/skein/MainActivity.kt
+++ Skein/Skein/src/main/java/ru/itstep/skein/MainActivity.kt
@@ -1,12 +1,21 @@
 package ru.itstep.skein
 
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,60 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        fusedLocationClient = turOnLocUpd()
+    }
+
+    
+    private fun turOnLocUpd(): FusedLocationProviderClient? {
+        var tv = findViewById<TextView>(R.id.stats)
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                    fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                    fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_BACKGROUND_LOCATION, false) -> {
+                    // Background location access granted.
+                    fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                } else -> {
+                // No location access granted.
+                    tv.text = "No permission"
+                    //return@registerForActivityResult
+                }
+            }
+        }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionRequest.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION))
+        } else {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED) {
+                locationPermissionRequest.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION))
+            }
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        }
+        return fusedLocationClient
     }
 }
