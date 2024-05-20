ea25dbd2876c1b6a5e8473eb8d267eecee0d2793
Wera Barinova <alisawera@yandex.ru>
2024-05-20 21:25:13 +0300

    Получены права для камеры

--- Retakepic/app/src/main/java/ru/itstep/retakepic/MainActivity.kt
+++ Retakepic/app/src/main/java/ru/itstep/retakepic/MainActivity.kt
@@ -1,8 +1,11 @@
 package ru.itstep.retakepic
 
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +13,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import ru.itstep.retakepic.ui.theme.RetakepicTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getCamOrNone()
         setContent {
             RetakepicTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +31,32 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun getCamOrNone() : Boolean {
+        val permissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.CAMERA, false) -> {
+                    // Precise location access granted.
+                    // fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                    } else -> {
+                    // No location access granted.
+                    //return@registerForActivityResult
+                }
+            }
+        }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionRequest.launch(arrayOf(
+                Manifest.permission.CAMERA))
+        } else {
+            return true
+        }
+        return false
     }
 }
 
